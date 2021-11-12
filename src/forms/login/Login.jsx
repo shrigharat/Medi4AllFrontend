5e0c1fd1f3c15912950398d895cd1f8c760c0581
Shrishail Gharat <gharatshri014@gmail.com>
2021-11-12 22:46:41 +0530

    finalised login form UI

--- src/forms/login/Login.jsx
+++ src/forms/login/Login.jsx
@@ -1,58 +1,57 @@
-import React from 'react'
-import "./Login.scss"
+import React, { useState } from "react";
+import "./Login.scss";
+import { Radio, RadioGroup, Stack } from "@chakra-ui/react";
+
+const userTypes = {
+  patient: "patient",
+  doctor: "doctor",
+  institute: "institute",
+};
 
 const LoginForm = () => {
-    return (
-        <section>
-
-            <div class="contentBx">
-                <div class="formBx">
-                    <h2>Login</h2>
-                    <form>
-                        <div class="auth-details">
-                            <input type="radio" name="auth" id="dot-1" />
-                            <input type="radio" name="auth" id="dot-2" />
-                            <input type="radio" name="auth" id="dot-3" />
-
-
-                            <span class="auth-titile">Select Identification</span>
-                            <div class="category">
-                                <label for="dot-1 ">
-                                    <span class="dot one"></span>
-                                    <span class="auth">User</span>
-                                </label>
-                                <label for="dot-2">
-                                    <span class="dot two"></span>
-                                    <span class="auth">Student</span>
-                                </label>
-                                <label for="dot-3">
-                                    <span class="dot three"></span>
-                                    <span class="auth">Institution</span>
-                                </label>
-                            </div>
-                        </div>
-                        <div class="inputBx">
-                            <span>Email Address or Username</span>
-                            <input type="text" name="" />
-                        </div>
-                        <div class="inputBx">
-                            <span>Password</span>
-                            <input type="password" name="" />
-                        </div>
-                        <div class="pass"><a href="#">Forgot password?</a></div>
-                        <div class="inputBx">
-                            <input type="submit" value="Log IN" name="" />
-                        </div>
-                        <div class="inputBx">
-                            <p>Don't have an account ?<a href="#">Register now</a></p>
-                        </div>
-                    </form>
-
-                </div>
+  const [userType, setUserType] = useState();
+  return (
+    <section>
+      <div class="contentBx">
+        <div class="formBx">
+          <h2>Login</h2>
+          <form>
+            <span class="auth-title">Select identitification type</span>
+            <RadioGroup
+              colorScheme="med"
+              onChange={setUserType}
+              value={userType}
+            >
+              <div className="radioContainer">
+                <Radio size="md" value={userTypes.patient}>User</Radio>
+                <Radio size="md" value={userTypes.doctor}>Doctor</Radio>
+                <Radio size="md" value={userTypes.institute}>Institute</Radio>
+              </div>
+            </RadioGroup>
+            <div class="inputBx">
+              <span>Email Address or Username</span>
+              <input type="text" name="" />
             </div>
-        </section>
+            <div class="inputBx">
+              <span>Password</span>
+              <input type="password" name="" />
+            </div>
+            <div class="pass">
+              <a href="#">Forgot password?</a>
+            </div>
+            <div class="inputBx">
+              <input type="submit" value="Log in" name="" />
+            </div>
+            <div class="inputBx">
+              <p className="noAccount">
+                Don't have an account ?<a href="#">Register now</a>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-    )
-}
-
-export default LoginForm
+export default LoginForm;
