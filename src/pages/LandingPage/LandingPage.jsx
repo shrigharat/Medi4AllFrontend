814601ca106b435fb9bae2c685318df27a4cfc9a
Shrishail Gharat <gharatshri014@gmail.com>
2021-11-19 02:29:25 +0530

    Modified landing page

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,37 +1,128 @@
 import React from "react";
 import classes from "./LandingPage.module.scss";
+import Medi4allLogo from "../../img/medi4alllogo.png";
+import TeleHealth from "../../img/tele-health.jpg";
+import PrescriptionImg from "../../img/20170523-prescription.jpg";
+import MedRecords from "../../img/medical-records-waf.jpg";
+import MedNews from "../../img/medical-news.jpg";
+import { Link, useHistory } from "react-router-dom";
 
 const LandingPage = () => {
+  const history = useHistory();
   return (
     <div className={classes.landing_page}>
       <header className={classes.lp_header}>
-        <a href="#" className={classes.logo}>
-          Medi4all
+        <a href="#home" className={classes.logo}>
+          <img className={classes.logo_img} src={Medi4allLogo} alt="" />
+          <span>
+            Medi<span className={classes.four}>4</span>all
+          </span>
         </a>
         <nav className={classes.navbar}>
-          <a href="#home">Home</a>
-          <a href="#aboutus">About Us</a>
-          <a href="#services">Services</a>
-          <a href="#login">Log In</a>
+          <a className={classes.nav_item} href="#home">
+            Home
+          </a>
+          <a className={classes.nav_item} href="#services">
+            Services
+          </a>
+          <a className={classes.nav_item} href="#aboutus">
+            About Us
+          </a>
+          <Link to="/login" className={classes.nav_item}>
+            Log In
+          </Link>
         </nav>
       </header>
-      <section className={classes.home} id="home">
-        <div className={classes.content}>
-          <h3>FUTURE IS HERE,</h3>
-          <h3>Manage Your Health</h3>
-          <h3>All At One Place!</h3>
-          <p>
-            "Our medical specialists care about you & your family's health"
-            <br />
-            Now your online account is a powerful tool for managing every aspect
-            of your health.
-          </p>
-          <a href="#" className={classes.btn}>
-            Register Now
-          </a>
+      <section className={classes.home_banner} id="home">
+        <div className={classes.home_overlay} />
+        <div className={classes.home_content}>
+          <h3>
+            Manage every aspect <br /> of your health information <br /> with
+            our services.
+          </h3>
+
+          <div className={classes.btn_container}>
+            <button
+              onClick={() => history.push("/registration")}
+              className={classes.reg_btn}
+            >
+              Register
+            </button>
+          </div>
         </div>
-        <div className={classes.image}>
-          <img src="laptop_mockup.png" alt="" />
+      </section>
+
+      <section className={classes.services} id="services">
+        <h1 className={classes.services_heading}>SERVICES</h1>
+        <div className={classes.box_container}>
+          <div className={classes.service_box}>
+            <div className={classes.srvimg_container}>
+              <img className={classes.service_img} src={TeleHealth} alt="" />
+            </div>
+            <div className={classes.srv_imgoverlay}/>
+            <h3 className={classes.title}>Online Consultations</h3>
+            <p className={classes.description}>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo,
+              nulla! Velit, et. Unde a dolorum consectetur officiis facere
+              magnam totam amet praesentium nesciunt? Facere, officia sapiente
+              quam vero doloremque sunt?
+            </p>
+            <a href="#" className={classes.btn}>
+              Read More
+            </a>
+          </div>
+          <div className={classes.service_box}>
+            <div className={classes.srvimg_container}>
+              <img
+                className={classes.service_img}
+                src={PrescriptionImg}
+                alt=""
+              />
+            </div>
+            <div className={classes.srv_imgoverlay}/>
+            <h3 className={classes.title}>Manage Prescriptions</h3>
+            <p className={classes.description}>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo,
+              nulla! Velit, et. Unde a dolorum consectetur officiis facere
+              magnam totam amet praesentium nesciunt? Facere, officia sapiente
+              quam vero doloremque sunt?
+            </p>
+            <a href="#" className={classes.btn}>
+              Read More
+            </a>
+          </div>
+          <div className={classes.service_box}>
+            <div className={classes.srvimg_container}>
+              <img className={classes.service_img} src={MedRecords} alt="" />
+            </div>
+            <div className={classes.srv_imgoverlay}/>
+            <h3 className={classes.title}>Track Medical Records</h3>
+            <p className={classes.description}>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo,
+              nulla! Velit, et. Unde a dolorum consectetur officiis facere
+              magnam totam amet praesentium nesciunt? Facere, officia sapiente
+              quam vero doloremque sunt?
+            </p>
+            <a href="#" className={classes.btn}>
+              Read More
+            </a>
+          </div>
+          <div className={classes.service_box}>
+            <div className={classes.srvimg_container}>
+              <img className={classes.service_img} src={MedNews} alt="" />
+            </div>
+            <div className={classes.srv_imgoverlay}/>
+            <h3 className={classes.title}>Read Health Articles</h3>
+            <p className={classes.description}>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo,
+              nulla! Velit, et. Unde a dolorum consectetur officiis facere
+              magnam totam amet praesentium nesciunt? Facere, officia sapiente
+              quam vero doloremque sunt?
+            </p>
+            <a href="#" className={classes.btn}>
+              Read More
+            </a>
+          </div>
         </div>
       </section>
 
@@ -57,64 +148,6 @@ const LandingPage = () => {
                 Read More
               </a>
             </div>
-          </div>
-        </div>
-      </section>
-
-      <section className={classes.services} id="services">
-        <h1 className={classes.heading}>SERVICES</h1>
-        <div className={classes["box-container"]}>
-          <div className={classes.box}>
-            <img src="tele-health.jpg" alt="" />
-            <h3>Online Consultations</h3>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo,
-              nulla! Velit, et. Unde a dolorum consectetur officiis facere
-              magnam totam amet praesentium nesciunt? Facere, officia sapiente
-              quam vero doloremque sunt?
-            </p>
-            <a href="#" className={classes.btn}>
-              Read More
-            </a>
-          </div>
-          <div className={classes.box}>
-            <img src="20170523-prescription.jpg" alt="" />
-            <h3>Manage Prescriptions</h3>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo,
-              nulla! Velit, et. Unde a dolorum consectetur officiis facere
-              magnam totam amet praesentium nesciunt? Facere, officia sapiente
-              quam vero doloremque sunt?
-            </p>
-            <a href="#" className={classes.btn}>
-              Read More
-            </a>
-          </div>
-          <div className={classes.btn}>
-            <img src="medical-records-waf.jpg" alt="" />
-            <h3>Track Medical Records</h3>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo,
-              nulla! Velit, et. Unde a dolorum consectetur officiis facere
-              magnam totam amet praesentium nesciunt? Facere, officia sapiente
-              quam vero doloremque sunt?
-            </p>
-            <a href="#" className={classes.btn}>
-              Read More
-            </a>
-          </div>
-          <div className={classes.box}>
-            <img src="medical-news.jpg" alt="" />
-            <h3>Read Health Articles</h3>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo,
-              nulla! Velit, et. Unde a dolorum consectetur officiis facere
-              magnam totam amet praesentium nesciunt? Facere, officia sapiente
-              quam vero doloremque sunt?
-            </p>
-            <a href="#" className={classes.btn}>
-              Read More
-            </a>
           </div>
         </div>
       </section>
