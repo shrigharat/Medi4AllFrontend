ddc50cbcecd7024f5ba64bf56938ff52aca975bf
Shrishail Gharat <gharatshri014@gmail.com>
2021-11-18 12:18:53 +0530

    Added manifest file in src folder

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 // const baseURL = "https://localhost:5000";
 
-const axiosInstance = axios.create({ baseURL: "http://localhost:5000" });
+const axiosInstance = axios.create({ baseURL: "https://medi4allbackend-grumpy-gnu-dj.eu-de.mybluemix.net/" });
 
 export default axiosInstance;
